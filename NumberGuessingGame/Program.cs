b725337ee0b0735f79e7389935d7af08a9f80712
Braydon Sutherland <BraydonSutherland@gmail.com>
2022-09-14 09:04:50 -0500

    final commit

--- NumberGuessingGame/Program.cs
+++ NumberGuessingGame/Program.cs
@@ -2,7 +2,7 @@
 bool keepPlaying = true;
 int min = 1;
 int max = 100;
-double coins = 4.00; 
+double coins = 4.00;
 int randomNumber;
 int userGuess;
 int guesses;
@@ -23,13 +23,13 @@ while (keepPlaying && coins > 0)
     Console.WriteLine();
     Console.Write("Please Enter a Coin! ");
     Console.ReadLine();
-    Console.Clear(); 
-    coins -= 1; 
-    
+    Console.Clear();
+    coins -= 1;
+
     while (userGuess != randomNumber)
     {   // Innner Loop Start //
         Console.WriteLine($"Number of Guesses: * {guesses} *");
-        Console.WriteLine(); 
+        Console.WriteLine();
         Console.Write($"Please Enter a Number between {min} and {max}: ");
         userGuess = int.Parse(Console.ReadLine());
         if (randomNumber > userGuess)
@@ -37,44 +37,50 @@ while (keepPlaying && coins > 0)
             Console.WriteLine();
             Console.WriteLine("Too low, try again. ");
             Console.WriteLine();
-            
+           
+
         }
         else if (randomNumber < userGuess)
         {
             Console.WriteLine();
             Console.WriteLine("Too high, try again. ");
             Console.WriteLine();
-            
+
         }
         guesses++;
         // Break out of Inner Loop //
     }
     // Outter Loop Continue // 
-    Console.Clear(); 
+    Console.Clear();
     Console.WriteLine("YOU WON THE GAME!!!!!!!");
     Console.WriteLine();
     Console.WriteLine($"You have {coins} coins left! ");
     Console.WriteLine();
     Console.Write("Would You Like to Continue(Y/N) ");
     userResponse = Console.ReadLine();
-    Console.Clear(); 
+    Console.Clear();
 
     if (userResponse?.ToUpper() == "Y")
     {
-        keepPlaying = true; 
+        keepPlaying = true;
     }
-    else if(userResponse?.ToUpper() == "N")
+    else if (userResponse?.ToUpper() == "N")
     {
         keepPlaying = false;
         Console.WriteLine();
         Console.Write("Sounds Good! See you next time!");
-        Console.ReadLine(); 
+        Console.ReadLine();
     }
     else
     {
-        Console.WriteLine("Please Enter a Valid Choice"); 
+        Console.WriteLine("Please Enter a Valid Choice");
     }
 
     // Break out of Outer Loop // 
 
 }
+
+
+
+
+
