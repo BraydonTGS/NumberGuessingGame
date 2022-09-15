ec424de4bee131f20cf772d46db83c5586a24867
Braydon Sutherland <BraydonSutherland@gmail.com>
2022-09-15 08:01:42 -0500

    added a feature for when the user runs out of coins, added some ascii art

--- NumberGuessingGame/Program.cs
+++ NumberGuessingGame/Program.cs
@@ -2,48 +2,50 @@
 bool keepPlaying = true;
 int min = 1;
 int max = 100;
-double coins = 4.00;
+double coins = 1.00;
 int randomNumber;
 int userGuess;
 int guesses;
 string userResponse;
 
-while (keepPlaying && coins > 0)
+
+
+while (keepPlaying & coins > 0)
 {
     // Outer Loop Start /
     Random rnd = new Random();
     randomNumber = rnd.Next(1, 101);
     userGuess = 0;
     guesses = 0;
-    Console.WriteLine("*********************");
-    Console.WriteLine("Number Guessing Game");
-    Console.WriteLine("*********************");
+    Console.WriteLine(@" +-+-+-+-+-+-+ +-+-+-+-+-+-+-+-+ +-+-+-+-+
+ |N|u|m|b|e|r| |G|u|e|s|s|i|n|g| |G|a|m|e|
+ +-+-+-+-+-+-+ +-+-+-+-+-+-+-+-+ +-+-+-+-+");
     Console.WriteLine();
-    Console.WriteLine($"You have {coins} coins ");
+    Console.WriteLine($"> You have {coins} coins ");
     Console.WriteLine();
-    Console.Write("Please Enter a Coin! ");
+    Console.Write("> Please Enter a Coin! ");
     Console.ReadLine();
     Console.Clear();
     coins -= 1;
 
     while (userGuess != randomNumber)
     {   // Innner Loop Start //
-        Console.WriteLine($"Number of Guesses: * {guesses} *");
+        Console.WriteLine($"> Number of Guesses: * {guesses} *");
         Console.WriteLine();
-        Console.Write($"Please Enter a Number between {min} and {max}: ");
+        Console.Write($"> Please Enter a Number between {min} and {max}: ");
         userGuess = int.Parse(Console.ReadLine());
         if (randomNumber > userGuess)
         {
             Console.WriteLine();
-            Console.WriteLine("Too low, try again. ");
+            Console.WriteLine("> Too low, try again. ");
             Console.WriteLine();
-           
+
 
         }
         else if (randomNumber < userGuess)
         {
             Console.WriteLine();
-            Console.WriteLine("Too high, try again. ");
+            Console.WriteLine("> Too high, try again. ");
             Console.WriteLine();
 
         }
@@ -52,15 +54,15 @@ while (keepPlaying && coins > 0)
     }
     // Outter Loop Continue // 
     Console.Clear();
-    Console.WriteLine("YOU WON THE GAME!!!!!!!");
+    Console.WriteLine($"> YOU WON THE GAME! Number of Guesses: {guesses}");
     Console.WriteLine();
-    Console.WriteLine($"You have {coins} coins left! ");
+    Console.WriteLine($"> You have {coins} coins left! ");
     Console.WriteLine();
-    Console.Write("Would You Like to Continue(Y/N) ");
+    Console.Write("> Would You Like to Continue(Y/N) ");
     userResponse = Console.ReadLine();
     Console.Clear();
 
-    if (userResponse?.ToUpper() == "Y")
+    if (userResponse?.ToUpper() == "Y" && coins > 0)
     {
         keepPlaying = true;
     }
@@ -68,17 +70,33 @@ while (keepPlaying && coins > 0)
     {
         keepPlaying = false;
         Console.WriteLine();
-        Console.Write("Sounds Good! See you next time!");
-        Console.ReadLine();
+        Console.Write("> Sounds Good! See you next time!");
+        Console.ReadKey();
+    }
+
+    else if (coins == 0)
+    {
+        keepPlaying = false;
+        Console.WriteLine(@" +-+-+-+-+ +-+-+-+-+
+ |G|a|m|e| |O|v|e|r|
+ +-+-+-+-+ +-+-+-+-+");
+        Console.WriteLine();
+        Console.Write("> OH NO! You are out of coins! ");
+        Console.ReadKey();
+
     }
     else
     {
-        Console.WriteLine("Please Enter a Valid Choice");
+        Console.WriteLine("> Please Enter a Valid Choice");
     }
 
     // Break out of Outer Loop // 
 
 }
+
+
+
+
 
 
 
